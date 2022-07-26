f9251fade09462f6dfead676b50bc36f5517144a
Gabriel Rodrigues <gabrirodri123@gmail.com>
2022-07-26 14:20:00 -0300

    alteração desafio 2

--- Desafio Java/Desafio 2/Program.java
+++ Desafio Java/Desafio 2/Program.java
@@ -1,25 +1,26 @@
-
 // Abaixo segue um exemplo de código que você pode ou não utilizar
 import java.util.*;
-import java.text.DecimalFormat;
 
-public class Program {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-
-    int casos = input.nextInt();
-    int cont = 0;
-    while (cont < casos) {
-
-      double a = input.nextDouble();
-      double b = input.nextDouble();
-      double c = input.nextDouble();
-      // TODO: complete os espaços em branco com sua solução para o problema
-
-      double media = ((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5);
-      DecimalFormat df = new DecimalFormat("###,###.0");
-      System.out.println(df.format(media));
-      cont++;
+public class Program
+{
+    public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+       
+        int casos = input.nextInt();
+        int cont = 0;
+        while (cont < casos){
+ 
+            double a = input.nextDouble();
+            double b = input.nextDouble();
+            double c = input.nextDouble();
+	   // TODO: complete os espaços em branco com sua solução para o problema
+	          
+	          double media = ((a*2)+(b*3)+(c*5))/(2+3+5);
+	          System.out.printf("%.1f \n",media);
+            cont++;
+        }
+        
+        
     }
-  }
-}
+    }
