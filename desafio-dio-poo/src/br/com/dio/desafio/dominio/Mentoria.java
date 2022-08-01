9f4d1cfeefddf86dcdb5ba0efe4949c2e7ce7355
Gabriel Rodrigues <gabrirodri123@gmail.com>
2022-08-01 14:28:45 -0300

    classes de dominio adicionado

---
+++ desafio-dio-poo/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,56 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    String titulo;
+    String descricao;
+    int cargaHoraria;
+    LocalDate data;
+
+    
+    public Mentoria() {
+    }
+
+	public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                ", data=" + data +
+                '}';
+    }
+}
